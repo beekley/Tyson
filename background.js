a72e2e482d04fbe3e39b1cee577a630211f45b25
Brett Beekley <brettbeekley@gmail.com>
2017-06-21 20:56:28 -0700

    Added search results to omnibox

--- background.js
+++ background.js
@@ -1,5 +1,29 @@
 //omnibox event handling and functionality
-//$$$$
 
+// background page
+//const bkg = chrome.extension.getBackgroundPage();
 //
-chrome.omnibox.
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+  const start = new Date()
+  chrome.bookmarks.search(text, (results) => {
+    const stars = results
+      .filter(result => {
+        return !!result.url
+      })
+      .map(result => {
+        return {
+          content: result.url,
+          description: 'test'
+        }
+      });
+    suggest(stars);
+    //console.log(stars);
+    
+    const end = new Date();
+    console.log("Time to print results:", (end - start));
+  })
+});
+
+chrome.omnibox.onInputEntered.addListener((text) => {
+  alert('inputEntered: ' + text);
+})
